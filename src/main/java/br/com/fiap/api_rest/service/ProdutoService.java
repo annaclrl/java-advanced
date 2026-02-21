8a45dd38b75694144bef8ee672e1b24398503811
Anna <annaclararussoluca@gmail.com>
2026-02-20 22:55:17 -0300

    criando ProdutoService com o crud completo

--- src/main/java/br/com/fiap/api_rest/service/ProdutoService.java
+++ src/main/java/br/com/fiap/api_rest/service/ProdutoService.java
@@ -1,4 +1,38 @@
 package br.com.fiap.api_rest.service;
 
+import br.com.fiap.api_rest.model.Produto;
+import br.com.fiap.api_rest.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
 public class ProdutoService {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    private Produto create(Produto produto){
+        return produtoRepository.save(produto);
+    }
+
+    private Produto read(UUID id) {
+        Optional<Produto> produto = produtoRepository.findById(id);
+        return produto.orElse(null);
+    }
+
+    private List<Produto> readAll(){
+        return produtoRepository.findAll();
+    }
+
+    private Produto update(Produto produto){
+        return produtoRepository.save(produto);
+    }
+
+    private void delete(UUID id){
+        produtoRepository.deleteById(id);
+    }
 }
