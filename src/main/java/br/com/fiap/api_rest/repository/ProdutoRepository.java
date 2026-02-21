91de837f82f9461699838fc4421cd3dbe6d9d17c
Anna <annaclararussoluca@gmail.com>
2026-02-20 22:54:37 -0300

    criando ProdutoRepository

--- src/main/java/br/com/fiap/api_rest/repository/ProdutoRepository.java
+++ src/main/java/br/com/fiap/api_rest/repository/ProdutoRepository.java
@@ -1,4 +1,12 @@
 package br.com.fiap.api_rest.repository;
 
-public interface ProdutoRepository {
+import br.com.fiap.api_rest.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, UUID> {
+
 }
